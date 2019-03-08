d924fd64f02ec5f9730dc2850c1e632a3c70bf58
sm_malik <shahid.ibex@yahoo.com>
2019-03-08 17:14:42 +0500

    db data labeling changed to accept the variables

--- data-labeling-db.py
+++ data-labeling-db.py
@@ -3,7 +3,6 @@ import sys
 import socket
 import mysql.connector
 from app import get_current_time
-
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
 import config
 
@@ -17,9 +16,7 @@ class Connection:
     """
 
     def __init__(self):
-        """
-        constructor for connection class
-        """
+        """ constructor for connection class """
         if socket.gethostbyname(socket.gethostname()) == '127.0.1.1':
             self.DATABASE_CONFIG = config.LOCAL_DATABASE_CONFIG
         else:
@@ -32,10 +29,7 @@ class Connection:
         self.db = self.DATABASE_CONFIG['dbname']
 
     def connect(self):
-        """
-        Create a mysql database connection
-        :return:
-        """
+        """ Create a mysql database connection        """
         con = mysql.connector.connect(
             host=self.host,
             user=self.user,
@@ -47,10 +41,7 @@ class Connection:
         return con
 
     def update_db_url_data(self, url_data):
-        """
-        Update URL in the in the table (Package_features) for verdict and verdict timestamp
-        :return:
-        """
+        """ Update URL in the in the table (Package_features) for verdict and verdict timestamp        """
         con = self.connect()
         update_query = "update %s set verdict='%s', verdict_update_timestamp='%s' where url_md5 like '%s';" % (
             self.table, url_data['verdict'], url_data['verdict_timestamp'], url_data['url_md5'])
@@ -61,8 +52,7 @@ class Connection:
 
     @staticmethod
     def execute_query(con, query):
-        """
-        Given any query and database connection, this function will execute the query and return the status True
+        """ Given any query and database connection, this function will execute the query and return the status True
         if successful
         """
         cursor = con.cursor()
@@ -138,13 +128,14 @@ def main():
     args = sys.argv
     if len(args) > 2 or len(args) < 2:
         print("Please provide the arguments in order as shown in example command")
-        print("\npython data-labeling.py v1\nor\npython data-labeling.py v2\n")
+        print("\npython data-labeling-db.py filepath \nor\npython data-labeling-db.py /home/shahid/v2\n")
         sys.exit(1)
     else:
         cur_dir = os.path.dirname(os.path.realpath(__file__))
         file_name = "urls_verdict_result"  # urls_verdict_result_v1 or urls_verdict_result_v2
         file_version = args[1]
-        file_path = cur_dir + '/lib/' + file_name + '_' + file_version + '.csv'
+        # file_path = cur_dir + '/lib/' + file_name + '_' + file_version + '.csv'
+        file_path = args[1]
         urls = URL()
 
         urls.read_url_file(file_path)
